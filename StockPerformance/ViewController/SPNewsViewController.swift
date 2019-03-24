a5f1dedb89b5186e7586b62bf4809ab9120258d4
Larry Nguyen <larrynguyen33@gmail.com>
2019-03-24 02:11:11 -0700

    add top news view to news tab

--- StockPerformance/ViewController/SPNewsViewController.swift
+++ StockPerformance/ViewController/SPNewsViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class SPNewsViewController: UIViewController {
     
+    weak var mainScrollView: UIScrollView!
+    weak var topView: NewsView!
     weak var financeCollection: UICollectionView!
     weak var energyCollection: UICollectionView!
     weak var techCollection: UICollectionView!
     weak var unilitiesCollection: UICollectionView!
     weak var healthcareCollection: UICollectionView!
     weak var consumerServicesCollection: UICollectionView!
+    
+    var displayedIndustries: [Industry] = []
 
+    override func loadView() {
+        super.loadView()
+        displayedIndustries = [.Finance,.Energy,.Technology,.Utilities,.ConsumerService,.Healthcare]
+        setupScrollView()
+        setupTopView()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViewController()
+        
+        self.mainScrollView.delegate = self
+    }
+    
+    fileprivate func setupScrollView(){
+        let mainScrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        mainScrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height * 2)
+        self.view.addSubview(mainScrollView)
+        self.mainScrollView = mainScrollView
+    }
+    
+    fileprivate func setupTopView(){
+        let topView = NewsView()
+        topView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height * 3/5)
+        self.mainScrollView.addSubview(topView)
+        NSLayoutConstraint.activate([
+            topView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            topView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            topView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor)
+        ])
+        self.topView = topView
     }
 
 }
@@ -41,4 +72,8 @@ extension SPNewsViewController: ViewControllerable {
     func setupViewController() {
         self.view.backgroundColor = AppColor.mainBackgroud.value
     }
+}
+
+extension SPNewsViewController: UIScrollViewDelegate {
+    
 }
